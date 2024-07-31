2ddf459ba9ef194718e2fb8e69ee6bf780a2be82
Isaque Roberto <isaquezulato@gmail.com>
2024-07-31 09:27:56 -0300

    addicted API

---
+++ tiao-pardinho-discografia/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\FaixaController;
+
+Route::get('albuns', [AlbumController::class, 'index']);
+Route::get('albuns/{id}', [AlbumController::class, 'show']);
+Route::post('albuns', [AlbumController::class, 'store']);
+Route::put('albuns/{id}', [AlbumController::class, 'update']);
+Route::delete('albuns/{id}', [AlbumController::class, 'delete']);
+
+Route::post('faixas', [FaixaController::class, 'store']);
+Route::delete('faixas/{id}', [FaixaController::class, 'delete']);
