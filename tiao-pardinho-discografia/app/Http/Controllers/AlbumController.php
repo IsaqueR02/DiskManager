d8461fdef34aa9ad9f2723146a0e4523b2ae037f
Isaque Roberto <isaquezulato@gmail.com>
2024-07-31 09:02:45 -0300

    reformando controles

--- tiao-pardinho-discografia/app/Http/Controllers/AlbumController.php
+++ tiao-pardinho-discografia/app/Http/Controllers/AlbumController.php
@@ -2,63 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Album;
 use Illuminate\Http\Request;
 
 class AlbumController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource. 
+     * tradução: Listagem do display das musicas do album
      */
     public function index()
     {
-        //
+        return Album::with('faixas')->get();
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
+     * tradução: Amazenagem do album
      */
     public function store(Request $request)
     {
-        //
+        $album = Album::create($request->all());
+        return response()->json($album, 201);
     }
 
     /**
      * Display the specified resource.
+     * tradução: Sistema de pesquisa da faixa especifica
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        return Album::with('faixas')->find($id);
     }
 
     /**
      * Update the specified resource in storage.
+     * tradução: Atualização do sistema
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $album = Album::findOrFail($id);
+        $album->update($request->all());
+        return response()->json($album, 200);
     }
 
     /**
      * Remove the specified resource from storage.
+     * Para deletar ou remover
      */
-    public function destroy(string $id)
+    public function delete($id)
     {
-        //
+        Album::findOrFail($id)->delete();
+        return response()->json(null, 204);
     }
 }
